926367551ae95a1218a42f6f06bdfb6b0424b5be
stepee sondawale <steffisondawale06@gmail.com>
2025-10-06 12:26:51 +0530

    i have practice different datattypes

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 console.log('Hello, World!') 
 // This is a simple JavaScript file
+
 console.log(9*8); 
+// This file demonstrates multiplication operation
+
+typeof "john"
+console.log(typeof "john");  //string
+
+typeof 123
+console.log(typeof 123);     //number
+
+typeof true
+console.log(typeof true);    //boolean
+
+typeof {}
+console.log(typeof {});      //object
+
+typeof []
+console.log(typeof []);      //object;
+
