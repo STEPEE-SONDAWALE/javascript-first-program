984d95155cd536fb28682c6ac7407a9006ab7478
stepee sondawale <steffisondawale06@gmail.com>
2025-10-17 19:22:14 +0530

    multiplication

--- Function/BasicProgram.js
+++ Function/BasicProgram.js
@@ -5,8 +5,9 @@ myFunction();
 
 
 // Function to compute the product of p1 and p2
-function myFunction(p1, p2) {
+function mul(p1, p2) {
   return p1 * p2;
 }
-let result = myFunction(4, 3);
+let result = mul(4, 3);
+console.log(result); // Outputs: 12
 
