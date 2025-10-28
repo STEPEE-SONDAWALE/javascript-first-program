7227731353a8c37227ad86b87eef65d6f430bb9f
stepee sondawale <steffisondawale06@gmail.com>
2025-10-28 21:15:23 +0530

    i have crete object folder.

---
+++ Objects/object.js
@@ -0,0 +1,8 @@
+ var std = {
+        name: "John",
+        age: 30,
+        no: 5613124,
+    }
+document.write("Name: " + std.name + "<br>");
+document.write("Age: " + std.age + "<br>");
+document.write("No: " + std.no + "<br>");
