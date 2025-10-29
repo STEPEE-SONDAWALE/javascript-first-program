3d22d1f4c1e3d6b7bd11f9d71a80076062d2b90f
stepee sondawale <steffisondawale06@gmail.com>
2025-10-29 09:10:42 +0530

    I add new file array

---
+++ array/array.js
@@ -0,0 +1,21 @@
+// taking output from user using prompt and storing in array
+// var fruit=[];
+// fruit[0]=prompt("enter the fruit name:");
+// fruit[1]=prompt("enter the fruit name:");
+// fruit[2]=prompt("enter the fruit name:");
+// document.write(fruit[0]+"<br>");
+// document.write(fruit[1]+"<br>");
+// document.write(fruit[2]+"<br>");
+
+// here we are defining array and accessing 3rd element of array
+var fruit=[];
+fruit[0]="apple";
+fruit[1]="banana";
+fruit[2]="mango";
+fruit[3]="grapes";
+fruit[4]="orange";
+document.write(fruit[2]+"<br>");
+document.write(fruit[3]+"<br>");
+document.write(fruit[4]+"<br>");
+
+document.write(fruit[0]+"<br>");
