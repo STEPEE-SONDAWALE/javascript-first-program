46d22409192c93a6662e12337043ce9ddfcc4f75
stepee sondawale <steffisondawale06@gmail.com>
2025-10-30 22:09:46 +0530

    more method

--- array/arrayMethod/method.js
+++ array/arrayMethod/method.js
@@ -1,9 +1,23 @@
 // *lengh 
-var n=["aa","bb","cc"];
-let b=n.length;
-document.write(b);
+// var n=["aa","bb","cc"];
+// let b=n.length;
+// document.write(b);
 
 //to string
-const k=[55,66,44,99];
-let h=k.toString();
-document.write(h)
+// const k=[55,66,44,99];
+// let h=k.toString();
+// document.write(h)
+
+//at
+// const m=[55,66,22,88,55,44];
+// let g=m.at(5);
+// document.write(g);
+
+//pop-thi delete the lat element from the array
+// const a=['mango','banana','apple','orange'];
+// let h=a.pop();
+// document.write(h);
+
+//push
+
+      
